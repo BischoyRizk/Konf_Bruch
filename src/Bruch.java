6aaa7e179401a0be26c77c9473d32c02162e2828
BischoyRizk <bischoy.rizk13@hotmail.com>
2021-10-23 14:05:16 +0200

    Multiplikationen der Brüche

--- src/Bruch.java
+++ src/Bruch.java
@@ -36,13 +36,18 @@ public class Bruch {
         *  Retournieren Sie ihre neue Bruch-Variable
         * */
 
-
-        return null;
+    Bruch b = new Bruch(this.numerator * b2.getNumerator(),
+            this.denominator * b2.getDenominator());
+        return b;
     }
 
     public Bruch multiplicate(Bruch b2,  Bruch b3) {
         // TODO: Multiplizieren Sie den eigenen Bruch mit b2 und b3
 
-        return null;
+        Bruch b = new Bruch(this.numerator * b2.getDenominator() * b3.getNumerator(),
+                this.denominator * b2.getDenominator() * b3.getDenominator());
+
+
+        return b;
     }
 }
